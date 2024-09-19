ae23a87d6a52f9789471acd818ab5ffd520aadbe
Sasa Nedic <svetasmak0@gmail.com>
2024-09-19 18:33:35 +0200

    Create testimonial-data

---
+++ src/utils/testimonialData.ts
@@ -0,0 +1,413 @@
+export const testimonialData = [
+    {
+        rank: 50,
+        title: 'alien',
+        thumbnail:
+          'https://m.media-amazon.com/images/M/MV5BOGQzZTBjMjQtOTVmMS00NGE5LWEyYmMtOGQ1ZGZjNmRkYjFhXkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_UX45_CR0,0,45,67_AL_.jpg',
+        rating: '8.4',
+        id: 'top50',
+        year: 1979,
+        image:
+          'https://m.media-amazon.com/images/M/MV5BOGQzZTBjMjQtOTVmMS00NGE5LWEyYmMtOGQ1ZGZjNmRkYjFhXkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_QL75_UX380_CR0,6,380,562_.jpg',
+        description:
+          'The crew of a commercial spacecraft encounter a deadly lifeform after investigating an unknown transmission.',
+        trailer: 'https://www.youtube.com/embed/jQ5lPt9edzQ',
+        genre: ['Horror', 'Sci-Fi'],
+        director: ['Ridley Scott'],
+        writers: ["Dan O'Bannon (screenplay by)", 'Ronald Shusett (story by)'],
+        imdbid: 'tt0078748',
+      },
+      {
+        rank: 51,
+        title: 'city lights',
+        thumbnail:
+          'https://m.media-amazon.com/images/M/MV5BY2I4MmM1N2EtM2YzOS00OWUzLTkzYzctNDc5NDg2N2IyODJmXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX45_CR0,0,45,67_AL_.jpg',
+        rating: '8.4',
+        id: 'top51',
+        year: 1931,
+        image:
+          'https://m.media-amazon.com/images/M/MV5BY2I4MmM1N2EtM2YzOS00OWUzLTkzYzctNDc5NDg2N2IyODJmXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_QL75_UX380_CR0,4,380,562_.jpg',
+        description:
+          'With the aid of a wealthy erratic tippler, a dewy-eyed tramp who has fallen in love with a sightless flower girl accumulates money to be able to help her medically.',
+        trailer: 'https://www.youtube.com/embed/7vl7F8S4cpQ',
+        genre: ['Comedy', 'Drama', 'Romance'],
+        director: ['Charles Chaplin'],
+        writers: [
+          'Charles Chaplin',
+          'Harry Carr (uncredited)',
+          'Harry Crocker (uncredited)',
+        ],
+        imdbid: 'tt0021749',
+      },
+      {
+        rank: 52,
+        title: 'cinema paradiso',
+        thumbnail:
+          'https://m.media-amazon.com/images/M/MV5BM2FhYjEyYmYtMDI1Yy00YTdlLWI2NWQtYmEzNzAxOGY1NjY2XkEyXkFqcGdeQXVyNTA3NTIyNDg@._V1_UY67_CR0,0,45,67_AL_.jpg',
+        rating: '8.4',
+        id: 'top52',
+        year: 1988,
+        image:
+          'https://m.media-amazon.com/images/M/MV5BM2FhYjEyYmYtMDI1Yy00YTdlLWI2NWQtYmEzNzAxOGY1NjY2XkEyXkFqcGdeQXVyNTA3NTIyNDg@._V1_QL75_UX380_CR0,0,380,562_.jpg',
+        description:
+          "A filmmaker recalls his childhood when falling in love with the pictures at the cinema of his home village and forms a deep friendship with the cinema's projectionist.",
+        trailer: 'https://www.youtube.com/embed/C2-GX0Tltgw',
+        genre: ['Drama', 'Romance'],
+        director: ['Giuseppe Tornatore'],
+        writers: [
+          'Giuseppe Tornatore (story)',
+          'Vanna Paoli (collaborating writer)',
+        ],
+        imdbid: 'tt0095765',
+      },
+      {
+        rank: 53,
+        title: 'apocalypse now',
+        thumbnail:
+          'https://m.media-amazon.com/images/M/MV5BYmQyNTA1ZGItNjZjMi00NzFlLWEzMWEtNWMwN2Q2MjJhYzEyXkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_UX45_CR0,0,45,67_AL_.jpg',
+        rating: '8.4',
+        id: 'top53',
+        year: 1979,
+        image:
+          'https://m.media-amazon.com/images/M/MV5BYmQyNTA1ZGItNjZjMi00NzFlLWEzMWEtNWMwN2Q2MjJhYzEyXkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_QL75_UX380_CR0,11,380,562_.jpg',
+        description:
+          'A U.S. Army officer serving in Vietnam is tasked with assassinating a renegade Special Forces Colonel who sees himself as a god.',
+        trailer: 'https://www.youtube.com/embed/FTjG-Aux_yQ',
+        genre: ['Drama', 'Mystery', 'War'],
+        director: ['Francis Ford Coppola'],
+        writers: [
+          'John Milius',
+          'Francis Ford Coppola',
+          'Michael Herr (narration)',
+        ],
+        imdbid: 'tt0078788',
+      },
+      {
+        rank: 54,
+        title: 'memento',
+        thumbnail:
+          'https://m.media-amazon.com/images/M/MV5BZTcyNjk1MjgtOWI3Mi00YzQwLWI5MTktMzY4ZmI2NDAyNzYzXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY67_CR0,0,45,67_AL_.jpg',
+        rating: '8.4',
+        id: 'top54',
+        year: 2000,
+        image:
+          'https://m.media-amazon.com/images/M/MV5BZTcyNjk1MjgtOWI3Mi00YzQwLWI5MTktMzY4ZmI2NDAyNzYzXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_QL75_UY562_CR2,0,380,562_.jpg',
+        description:
+          "A man with short-term memory loss attempts to track down his wife's murderer.",
+        trailer: 'https://www.youtube.com/embed/4CV41hoyS8A',
+        genre: ['Mystery', 'Thriller'],
+        director: ['Christopher Nolan'],
+        writers: [
+          'Christopher Nolan (screenplay)',
+          'Jonathan Nolan (short story "Memento Mori")',
+        ],
+        imdbid: 'tt0209144',
+      },
+      {
+        rank: 55,
+        title: 'indiana jones and the raiders of the lost ark',
+        thumbnail:
+          'https://m.media-amazon.com/images/M/MV5BNTU2ODkyY2MtMjU1NC00NjE1LWEzYjgtMWQ3MzRhMTE0NDc0XkEyXkFqcGdeQXVyMjM4MzQ4OTQ@._V1_UY67_CR0,0,45,67_AL_.jpg',
+        rating: '8.4',
+        id: 'top55',
+        year: 1981,
+        image:
+          'https://m.media-amazon.com/images/M/MV5BNTU2ODkyY2MtMjU1NC00NjE1LWEzYjgtMWQ3MzRhMTE0NDc0XkEyXkFqcGdeQXVyMjM4MzQ4OTQ@._V1_QL75_UY562_CR1,0,380,562_.jpg',
+        description:
+          'Archaeology professor Indiana Jones ventures to seize a biblical artefact known as the Ark of the Covenant. While doing so, he puts up a fight against Renee and a troop of Nazis.',
+        trailer: 'https://www.youtube.com/embed/Rh_BJXG1-44',
+        genre: ['Action', 'Adventure'],
+        director: ['Steven Spielberg'],
+        writers: [
+          'Lawrence Kasdan (screenplay by)',
+          'George Lucas (story by)',
+          'Philip Kaufman (story by)',
+        ],
+        imdbid: 'tt0082971',
+      },
+      {
+        rank: 56,
+        title: 'django unchained',
+        thumbnail:
+          'https://m.media-amazon.com/images/M/MV5BMjIyNTQ5NjQ1OV5BMl5BanBnXkFtZTcwODg1MDU4OA@@._V1_UY67_CR0,0,45,67_AL_.jpg',
+        rating: '8.4',
+        id: 'top56',
+        year: 2012,
+        image:
+          'https://m.media-amazon.com/images/M/MV5BMjIyNTQ5NjQ1OV5BMl5BanBnXkFtZTcwODg1MDU4OA@@._V1_QL75_UX380_CR0,0,380,562_.jpg',
+        description:
+          'With the help of a German bounty-hunter, a freed slave sets out to rescue his wife from a brutal plantation-owner in Mississippi.',
+        trailer: 'https://www.youtube.com/embed/0fUCuvNlOCg',
+        genre: ['Drama', 'Western'],
+        director: ['Quentin Tarantino'],
+        writers: ['Quentin Tarantino'],
+        imdbid: 'tt1853728',
+      },
+      {
+        rank: 57,
+        title: 'WALL·E',
+        thumbnail:
+          'https://m.media-amazon.com/images/M/MV5BMjExMTg5OTU0NF5BMl5BanBnXkFtZTcwMjMxMzMzMw@@._V1_UY67_CR0,0,45,67_AL_.jpg',
+        rating: '8.4',
+        id: 'top57',
+        year: 2008,
+        image:
+          'https://m.media-amazon.com/images/M/MV5BMjExMTg5OTU0NF5BMl5BanBnXkFtZTcwMjMxMzMzMw@@._V1_QL75_UX380_CR0,0,380,562_.jpg',
+        description:
+          'In the distant future, a small waste-collecting robot inadvertently embarks on a space journey that will ultimately decide the fate of mankind.',
+        trailer: 'https://www.youtube.com/embed/CZ1CATNbXg0',
+        genre: ['Animation', 'Adventure', 'Family'],
+        director: ['Andrew Stanton'],
+        writers: [
+          'Andrew Stanton (original story by)',
+          'Pete Docter (original story by)',
+          'Jim Reardon (screenplay by)',
+        ],
+        imdbid: 'tt0910970',
+      },
+      {
+        rank: 58,
+        title: 'the lives of others',
+        thumbnail:
+          'https://m.media-amazon.com/images/M/MV5BNmQyNmJjM2ItNTQzYi00ZjMxLWFjMDYtZjUyN2YwZDk5YWQ2XkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_UX45_CR0,0,45,67_AL_.jpg',
+        rating: '8.4',
+        id: 'top58',
+        year: 2006,
+        image:
+          'https://m.media-amazon.com/images/M/MV5BNmQyNmJjM2ItNTQzYi00ZjMxLWFjMDYtZjUyN2YwZDk5YWQ2XkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_QL75_UX380_CR0,3,380,562_.jpg',
+        description:
+          'In 1984 East Berlin, an agent of the secret police, conducting surveillance on a writer and his lover, finds himself becoming increasingly absorbed by their lives.',
+        trailer: 'https://www.youtube.com/embed/YsShZNHmpGE',
+        genre: ['Drama', 'Mystery', 'Thriller'],
+        director: ['Florian Henckel von Donnersmarck'],
+        writers: ['Florian Henckel von Donnersmarck'],
+        imdbid: 'tt0405094',
+      },
+      {
+        rank: 59,
+        title: 'sunset blvd.',
+        thumbnail:
+          'https://m.media-amazon.com/images/M/MV5BMTU0NTkyNzYwMF5BMl5BanBnXkFtZTgwMDU0NDk5MTI@._V1_UX45_CR0,0,45,67_AL_.jpg',
+        rating: '8.4',
+        id: 'top59',
+        year: 1950,
+        image:
+          'https://m.media-amazon.com/images/M/MV5BMTU0NTkyNzYwMF5BMl5BanBnXkFtZTgwMDU0NDk5MTI@._V1_QL75_UX380_CR0,5,380,562_.jpg',
+        description:
+          'A screenwriter develops a dangerous relationship with a faded film star determined to make a triumphant return.',
+        trailer: 'https://www.youtube.com/embed/_dY0SVxnHjQ',
+        genre: ['Drama', 'Film-Noir'],
+        director: ['Billy Wilder'],
+        writers: ['Charles Brackett', 'Billy Wilder', 'D.M. Marshman Jr.'],
+        imdbid: 'tt0043014',
+      },
+      {
+        rank: 60,
+        title: 'paths of glory',
+        thumbnail:
+          'https://m.media-amazon.com/images/M/MV5BOTI5Nzc0OTMtYzBkMS00NjkxLThmM2UtNjM2ODgxN2M5NjNkXkEyXkFqcGdeQXVyNjQ2MjQ5NzM@._V1_UX45_CR0,0,45,67_AL_.jpg',
+        rating: '8.4',
+        id: 'top60',
+        year: 1957,
+        image:
+          'https://m.media-amazon.com/images/M/MV5BOTI5Nzc0OTMtYzBkMS00NjkxLThmM2UtNjM2ODgxN2M5NjNkXkEyXkFqcGdeQXVyNjQ2MjQ5NzM@._V1_QL75_UX380_CR0,10,380,562_.jpg',
+        description:
+          'After refusing to attack an enemy position, a general accuses the soldiers of cowardice and their commanding officer must defend them.',
+        trailer: 'https://www.youtube.com/embed/AV9XLjDbt5A',
+        genre: ['Drama', 'War'],
+        director: ['Stanley Kubrick'],
+        writers: [
+          'Stanley Kubrick (screenplay)',
+          'Calder Willingham (screenplay)',
+          'Jim Thompson (screenplay)',
+        ],
+        imdbid: 'tt0050825',
+      },
+      {
+        rank: 61,
+        title: 'the shining',
+        thumbnail:
+          'https://m.media-amazon.com/images/M/MV5BZWFlYmY2MGEtZjVkYS00YzU4LTg0YjQtYzY1ZGE3NTA5NGQxXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX45_CR0,0,45,67_AL_.jpg',
+        rating: '8.4',
+        id: 'top61',
+        year: 1980,
+        image:
+          'https://m.media-amazon.com/images/M/MV5BZWFlYmY2MGEtZjVkYS00YzU4LTg0YjQtYzY1ZGE3NTA5NGQxXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_QL75_UX380_CR0,8,380,562_.jpg',
+        description:
+          'A family heads to an isolated hotel for the winter where a sinister presence influences the father into violence, while his psychic son sees horrific forebodings from both past and future.',
+        trailer: 'https://www.youtube.com/embed/FZQvIJxG9Xs',
+        genre: ['Drama', 'Horror'],
+        director: ['Stanley Kubrick'],
+        writers: [
+          'Stephen King (based upon the novel by)',
+          'Stanley Kubrick (screenplay by)',
+          'Diane Johnson (screenplay by)',
+        ],
+        imdbid: 'tt0081505',
+      },
+      {
+        rank: 62,
+        title: 'the great dictator',
+        thumbnail:
+          'https://m.media-amazon.com/images/M/MV5BMmExYWJjNTktNGUyZS00ODhmLTkxYzAtNWIzOGEyMGNiMmUwXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY67_CR0,0,45,67_AL_.jpg',
+        rating: '8.4',
+        id: 'top62',
+        year: 1940,
+        image:
+          'https://m.media-amazon.com/images/M/MV5BMmExYWJjNTktNGUyZS00ODhmLTkxYzAtNWIzOGEyMGNiMmUwXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_QL75_UY562_CR0,0,380,562_.jpg',
+        description:
+          "Dictator Adenoid Hynkel tries to expand his empire while a poor Jewish barber tries to avoid persecution from Hynkel's regime.",
+        trailer: 'https://www.youtube.com/embed/k8bVG8XC-4I',
+        genre: ['Comedy', 'Drama', 'War'],
+        director: ['Charles Chaplin'],
+        writers: ['Charles Chaplin'],
+        imdbid: 'tt0032553',
+      },
+      {
+        rank: 63,
+        title: 'avengers: infinity war',
+        thumbnail:
+          'https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_UY67_CR0,0,45,67_AL_.jpg',
+        rating: '8.4',
+        id: 'top63',
+        year: 2018,
+        image:
+          'https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_QL75_UX380_CR0,0,380,562_.jpg',
+        description:
+          'The Avengers and their allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe.',
+        trailer: 'https://www.youtube.com/embed/B65hW9YYY5A',
+        genre: ['Action', 'Adventure', 'Sci-Fi'],
+        director: ['Anthony Russo', 'Joe Russo'],
+        writers: [
+          'Christopher Markus (screenplay by)',
+          'Stephen McFeely (screenplay by)',
+          'Stan Lee (based on the Marvel comics by)',
+        ],
+        imdbid: 'tt4154756',
+      },
+      {
+        rank: 64,
+        title: 'witness for the prosecution',
+        thumbnail:
+          'https://m.media-amazon.com/images/M/MV5BNDQwODU5OWYtNDcyNi00MDQ1LThiOGMtZDkwNWJiM2Y3MDg0XkEyXkFqcGdeQXVyMDI2NDg0NQ@@._V1_UX45_CR0,0,45,67_AL_.jpg',
+        rating: '8.4',
+        id: 'top64',
+        year: 1957,
+        image:
+          'https://m.media-amazon.com/images/M/MV5BNDQwODU5OWYtNDcyNi00MDQ1LThiOGMtZDkwNWJiM2Y3MDg0XkEyXkFqcGdeQXVyMDI2NDg0NQ@@._V1_QL75_UX380_CR0,10,380,562_.jpg',
+        description:
+          'A veteran British barrister must defend his client in a murder trial that has surprise after surprise.',
+        trailer: 'https://www.youtube.com/embed/GMlJfiA2u7Y',
+        genre: ['Crime', 'Drama', 'Mystery'],
+        director: ['Billy Wilder'],
+        writers: [
+          "Agatha Christie (in Agatha Christie's international stage success)",
+          'Billy Wilder (screen play)',
+          'Harry Kurnitz (screen play)',
+        ],
+        imdbid: 'tt0051201',
+      },
+      {
+        rank: 65,
+        title: 'aliens',
+        thumbnail:
+          'https://m.media-amazon.com/images/M/MV5BZGU2OGY5ZTYtMWNhYy00NjZiLWI0NjUtZmNhY2JhNDRmODU3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX45_CR0,0,45,67_AL_.jpg',
+        rating: '8.3',
+        id: 'top65',
+        year: 1986,
+        image:
+          'https://m.media-amazon.com/images/M/MV5BZGU2OGY5ZTYtMWNhYy00NjZiLWI0NjUtZmNhY2JhNDRmODU3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_QL75_UX380_CR0,14,380,562_.jpg',
+        description:
+          'Fifty-seven years after surviving an apocalyptic attack aboard her space vessel by merciless space creatures, Officer Ripley awakens from hyper-sleep and tries to warn anyone who will listen about the predators.',
+        trailer: 'https://www.youtube.com/embed/oSeQQlaCZgU',
+        genre: ['Action', 'Adventure', 'Sci-Fi'],
+        director: ['James Cameron'],
+        writers: [
+          'James Cameron (story by)',
+          'David Giler (story by)',
+          'Walter Hill (story by)',
+        ],
+        imdbid: 'tt0090605',
+      },
+      {
+        rank: 66,
+        title: 'american beauty',
+        thumbnail:
+          'https://m.media-amazon.com/images/M/MV5BNTBmZWJkNjctNDhiNC00MGE2LWEwOTctZTk5OGVhMWMyNmVhXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UY67_CR0,0,45,67_AL_.jpg',
+        rating: '8.3',
+        id: 'top66',
+        year: 1999,
+        image:
+          'https://m.media-amazon.com/images/M/MV5BNTBmZWJkNjctNDhiNC00MGE2LWEwOTctZTk5OGVhMWMyNmVhXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_QL75_UX380_CR0,0,380,562_.jpg',
+        description:
+          "A sexually frustrated suburban father has a mid-life crisis after becoming infatuated with his daughter's best friend.",
+        trailer: 'https://www.youtube.com/embed/Ly7rq5EsTC8',
+        genre: ['Drama'],
+        director: ['Sam Mendes'],
+        writers: ['Alan Ball'],
+        imdbid: 'tt0169547',
+      },
+      {
+        rank: 67,
+        title: 'spider-man: into the spider-verse',
+        thumbnail:
+          'https://m.media-amazon.com/images/M/MV5BMjMwNDkxMTgzOF5BMl5BanBnXkFtZTgwNTkwNTQ3NjM@._V1_UY67_CR0,0,45,67_AL_.jpg',
+        rating: '8.3',
+        id: 'top67',
+        year: 2018,
+        image:
+          'https://m.media-amazon.com/images/M/MV5BMjMwNDkxMTgzOF5BMl5BanBnXkFtZTgwNTkwNTQ3NjM@._V1_QL75_UX380_CR0,1,380,562_.jpg',
+        description:
+          'Teen Miles Morales becomes the Spider-Man of his universe, and must join with five spider-powered individuals from other dimensions to stop a threat for all realities.',
+        trailer: 'https://www.youtube.com/embed/g4Hbz2jLxvQ',
+        genre: ['Animation', 'Action', 'Adventure'],
+        director: ['Bob Persichetti', 'Peter Ramsey', 'Rodney Rothman'],
+        writers: ['Phil Lord (screenplay by)', 'Rodney Rothman (screenplay by)'],
+        imdbid: 'tt4633694',
+      },
+      {
+        rank: 68,
+        title:
+          'dr. strangelove or: how i learned to stop worrying and love the bomb',
+        thumbnail:
+          'https://m.media-amazon.com/images/M/MV5BZWI3ZTMxNjctMjdlNS00NmUwLWFiM2YtZDUyY2I3N2MxYTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX45_CR0,0,45,67_AL_.jpg',
+        rating: '8.3',
+        id: 'top68',
+        year: 1964,
+        image:
+          'https://m.media-amazon.com/images/M/MV5BZWI3ZTMxNjctMjdlNS00NmUwLWFiM2YtZDUyY2I3N2MxYTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_QL75_UX380_CR0,14,380,562_.jpg',
+        description:
+          'An insane American general orders a bombing attack on the Soviet Union, triggering a path to nuclear holocaust that a war room full of politicians and generals frantically tries to stop.',
+        trailer: 'https://www.youtube.com/embed/jPU1AYTxwg4',
+        genre: ['Comedy', 'War'],
+        director: ['Stanley Kubrick'],
+        writers: [
+          'Stanley Kubrick (screenplay)',
+          'Terry Southern (screenplay)',
+          'Peter George (screenplay)',
+        ],
+        imdbid: 'tt0057012',
+      },
+      {
+        rank: 69,
+        title: 'the dark knight rises',
+        thumbnail:
+          'https://m.media-amazon.com/images/M/MV5BMTk4ODQzNDY3Ml5BMl5BanBnXkFtZTcwODA0NTM4Nw@@._V1_UY67_CR0,0,45,67_AL_.jpg',
+        rating: '8.3',
+        id: 'top69',
+        year: 2012,
+        image:
+          'https://m.media-amazon.com/images/M/MV5BMTk4ODQzNDY3Ml5BMl5BanBnXkFtZTcwODA0NTM4Nw@@._V1_QL75_UX380_CR0,0,380,562_.jpg',
+        description:
+          "Eight years after the Joker's reign of anarchy, Batman, with the help of the enigmatic Selina Kyle, is forced from his exile to save Gotham City from the brutal guerrilla terrorist Bane.",
+        trailer: 'https://www.youtube.com/embed/GokKUqLcvD8',
+        genre: ['Action', 'Drama'],
+        director: ['Christopher Nolan'],
+        writers: [
+          'Jonathan Nolan (screenplay)',
+          'Christopher Nolan (screenplay)',
+          'David S. Goyer (story)',
+        ],
+        imdbid: 'tt1345836',
+      },
+]
