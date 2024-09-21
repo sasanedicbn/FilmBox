8caf7691fa7034086050c0b438d524e6ec8bfffc
Sasa Nedic <svetasmak0@gmail.com>
2024-09-21 15:40:31 +0200

    Create changes

--- src/components/Home/testimonial/TitleTestimonial.tsx
+++ src/components/Home/testimonial/TitleTestimonial.tsx
@@ -1,7 +1,7 @@
 const TitleTestimonial = () => {
     return(
-        <div className="flex justify-between ">
-            <p>Currently trending</p>
+        <div className="flex justify-between mb-2 ">
+            <p className="text-lg font-medium text-white">Currently trending</p>
             <div className="flex justify-end gap-1 p-2">
                 <div className="pt-1 px-3 bg-cyan-200"></div>
                 <div className="pt-1 px-3 bg-cyan-200"></div>
