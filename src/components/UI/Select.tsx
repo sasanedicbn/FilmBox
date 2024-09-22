d1d0ff926354e2394b2cc707a6d888ca528e6869
Sasa Nedic <svetasmak0@gmail.com>
2024-09-22 23:59:51 +0200

    Create Select component

---
+++ src/components/UI/Select.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react";
+
+interface SelectProps {
+    name: string;
+    onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+    children: React.ReactNode; // Select će primati Option komponente kao decu
+    value?: string;
+    className?: string;
+}
+
+const Select = ({ name, onChange, children, value, className }:SelectProps) => {
+    return (
+        <select
+            name={name}
+            value={value}
+            onChange={onChange}
+            className={`p-2 border border-gray-300 rounded-lg ${className}`}
+        >
+            {children}
+        </select>
+    );
+};
+
+export default Select;
