d5c9993e2e4f774090244f2eda5de07f216763ce
Sasa Nedic <svetasmak0@gmail.com>
2024-09-23 22:38:32 +0200

    Working on Hover component

--- src/components/Home/testimonial/CardTestimonial.tsx
+++ src/components/Home/testimonial/CardTestimonial.tsx
@@ -1,7 +1,10 @@
+import HoverComponent from "../../UI/HoverComponent";
+
 const CardTestimonial = ({ testimonialFilms }) => {
   return (
-      <div className="w-full h-80 bg-gray-900 flex justify-center items-center overflow-hidden">
+      <div className="w-full h-80 bg-gray-900 flex justify-center items-center overflow-hidden relative">
           <img className="max-w-full h-80 object-cover " src={testimonialFilms.image} alt="Testimonial" />
+          <HoverComponent/>
       </div>
   );
 };
--- src/components/UI/HoverComponent.tsx
+++ src/components/UI/HoverComponent.tsx
@@ -1,11 +1,14 @@
 import Icon from "./Icon";
 
 const HoverComponent = () => {
-    return(
-        <div>
-           <span> <Icon name="marked" size={30} color="grey" /></span>
-           <p>See more <span><Icon name="search" size={30} color="grey" /></span></p>
+    return (
+      <div className="absolute  inset-0 bg-slate-400 mx-7 pointer bg-opacity-40 flex flex-col justify-center items-center opacity-0 hover:opacity-100 transition-opacity duration-300 p-2">
+        <div className="text-center ">
+          <span className="absolute top-2 right-2"><Icon name="marked" size={30} color="white" /></span>
+          <p className="mt-4 text-white">See more <span><Icon name="search" size={30} color="grey" /></span></p>
         </div>
-    )
-}
-export default HoverComponent;
+      </div>
+    );
+  };
+  
+  export default HoverComponent;
