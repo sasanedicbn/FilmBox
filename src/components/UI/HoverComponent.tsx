4a49259a6399e8eab3104520284bbf3ab415b532
Sasa Nedic <svetasmak0@gmail.com>
2024-09-23 23:59:41 +0200

    Finish HoverComponenet

--- src/components/UI/HoverComponent.tsx
+++ src/components/UI/HoverComponent.tsx
@@ -2,10 +2,10 @@ import Icon from "./Icon";
 
 const HoverComponent = () => {
     return (
-      <div className="absolute  inset-0 bg-slate-400 mx-7 pointer bg-opacity-40 flex flex-col justify-center items-center opacity-0 hover:opacity-100 transition-opacity duration-300 p-2">
+      <div className="absolute  inset-0 bg-slate-400 mx-7 pointer bg-opacity-80 flex flex-col justify-center items-center opacity-0 hover:opacity-100 transition-opacity duration-300 p-2">
         <div className="text-center ">
           <span className="absolute top-2 right-2"><Icon name="marked" size={30} color="white" /></span>
-          <p className="mt-4 text-white">See more <span><Icon name="search" size={30} color="grey" /></span></p>
+          <p className="mt-4 text-gray flex gap-2 items-center justify-center bg-gray-300 bg-opacity-80 p-2">See more <span><Icon name="search" size={15} color="black" /></span></p>
         </div>
       </div>
     );
