4ac2fc94c40cf105c3f22330599f2ca7254ac89c
Sasa Nedic <svetasmak0@gmail.com>
2024-10-01 00:23:32 +0200

    Create changes in HoverComponent

--- src/components/UI/HoverComponent.tsx
+++ src/components/UI/HoverComponent.tsx
@@ -4,22 +4,22 @@ import { doc, getDoc } from "firebase/firestore";
 import { db } from "../../config/firebase";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { setCurrentFilm } from "../../store/slices/filmsSlice";
+import { setCurrentFilm, addMarketFilms } from "../../store/slices/filmsSlice"; 
 import { Film } from "../../types/types";
 
-const HoverComponent = ({ films }:{films:Film}) => {
+const HoverComponent = ({ films }: { films: Film }) => {
   const navigate = useNavigate();
-  const dispatch = useDispatch(); 
+  const dispatch = useDispatch();
 
-  const fetchFilmById = async (filmId:string) => {
+  const fetchFilmById = async (filmId: string) => {
     try {
-      const filmRef = doc(db, 'testimonialFilms', filmId);
+      const filmRef = doc(db, "testimonialFilms", filmId);
       const filmSnap = await getDoc(filmRef);
 
       if (filmSnap.exists()) {
         const filmData = { id2: filmSnap.id, ...filmSnap.data() };
-        dispatch(setCurrentFilm(filmData)); 
-        navigate(`/home/${filmId}`); 
+        dispatch(setCurrentFilm(filmData));
+        navigate(`/home/${filmId}`);
         return filmData;
       } else {
         return null;
@@ -30,14 +30,21 @@ const HoverComponent = ({ films }:{films:Film}) => {
     }
   };
 
+  const handleMarkFilm = () => {
+    dispatch(addMarketFilms(films)); 
+    toast.success("Film marked successfully!"); 
+  };
+
   return (
     <div className="absolute inset-0 bg-slate-400 mx-6 pointer bg-opacity-60 flex flex-col justify-center items-center opacity-0 hover:opacity-100 transition-opacity duration-300 p-2">
       <div className="text-center">
         <span className="absolute top-2 right-2">
-          <Icon name="marked" size={30} color="white" />
+          <Icon name="marked" size={30} color="white" onClick={handleMarkFilm} /> 
         </span>
-        <p className="mt-4 text-gray-600 flex gap-2 items-center justify-center cursor-pointer bg-gray-300 bg-opacity-80 py-2 px-3 rounded-2xl hover:bg-gray-200 font-semibold transition-all duration-300" 
-           onClick={() => fetchFilmById(films.id2)}>
+        <p
+          className="mt-4 text-gray-600 flex gap-2 items-center justify-center cursor-pointer bg-gray-300 bg-opacity-80 py-2 px-3 rounded-2xl hover:bg-gray-200 font-semibold transition-all duration-300"
+          onClick={() => fetchFilmById(films.id2)}
+        >
           See more <span><Icon name="search" size={15} color="gray" /></span>
         </p>
       </div>
