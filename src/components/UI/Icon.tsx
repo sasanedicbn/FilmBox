111d437c236ddb0c8a47ef50eb5f68ae7d85d3a4
Sasa Nedic <svetasmak0@gmail.com>
2024-10-01 00:37:36 +0200

    Create changes in Icon component

--- src/components/UI/Icon.tsx
+++ src/components/UI/Icon.tsx
@@ -1,12 +1,12 @@
 import { FaBookmark, FaHome, FaSearch, FaStar } from 'react-icons/fa';
 import { IconProps } from '../../types/types';
 
-const Icon = ({ name, size = 84, color = 'black' }: IconProps) => {
+const Icon = ({ name, size = 84, color = 'black', onClick }: IconProps) => {
   const icons = {
-    marked: <FaBookmark size={size} color={color} />,
-    home: <FaHome size={size} color={color} />,
-    search: <FaSearch size={size} color={color} />,
-    star: <FaStar color={color} size={size} />
+    marked: <FaBookmark size={size} color={color} onClick={onClick ? onClick : undefined} />, 
+    home: <FaHome size={size} color={color} />, 
+    search: <FaSearch size={size} color={color} onClick={onClick ? onClick : undefined} />, 
+    star: <FaStar color={color} size={size} />, 
   };
 
   return icons[name] || null;
