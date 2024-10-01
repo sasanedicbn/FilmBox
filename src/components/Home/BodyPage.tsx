ab34889d079427ffedeeb7db0621c28d4e8dcce4
Sasa Nedic <svetasmak0@gmail.com>
2024-10-01 17:47:12 +0200

    Create changes in BodyPage and one more component

--- src/components/Home/BodyPage.tsx
+++ src/components/Home/BodyPage.tsx
@@ -3,31 +3,46 @@ import Icon from "../UI/Icon";
 import Select from "../UI/Select";
 import Option from "../UI/Option";
 
-const BodyPage = () => {
-    return (
-        <div className="bg-gray-800 mx-auto flex items-center justify-between w-[75%]  mt-24 p-4  rounded-lg">
-            <Input
-                type="text"
-                name="searchFilms"
-                placeholder="Search films..."
-                onChange={() => { console.log('onchange') }}
-                styleType='searchFilms'
-                value=""
-            />
+const BodyPage = ({ openClickedFilms, openFilms }) => {
+  return (
+    <div className="bg-gray-800 mx-auto flex items-center justify-between w-[75%] mt-24 p-4 rounded-lg">
+      <Input
+        type="text"
+        name="searchFilms"
+        placeholder="Search films..."
+        onChange={() => {
+          console.log("onchange");
+        }}
+        styleType="searchFilms"
+        value=""
+      />
 
-            <div className="flex items-center gap-4">
-                <Select name="genreSelect" onChange={(e) => console.log(e.target.value)}>
-                    <Option value="all">All</Option>
-                    <Option value="action">Action</Option>
-                    <Option value="drama">Drama</Option>
-                    <Option value="comedy">Comedy</Option>
-                    <Option value="horror">Horror</Option>
-                </Select>
-                <Icon name="marked" size={30} color="grey" />
-                <Icon name="home" size={30} color="grey" />
-            </div>
-        </div>
-    );
+      <div className="flex items-center gap-4">
+        <Select
+          name="genreSelect"
+          onChange={(e) => console.log(e.target.value)}
+        >
+          <Option value="all">All</Option>
+          <Option value="action">Action</Option>
+          <Option value="drama">Drama</Option>
+          <Option value="comedy">Comedy</Option>
+          <Option value="horror">Horror</Option>
+        </Select>
+        <Icon
+          name="marked"
+          size={30}
+          color={openFilms ? "red" : "grey"}
+          onClick={openClickedFilms}
+        />
+        <Icon
+          name="home"
+          size={30}
+          color={openFilms ? "grey" : "red"}
+          onClick={openClickedFilms}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default BodyPage;
--- src/components/Home/MainPage.tsx
+++ src/components/Home/MainPage.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { paginations } from "../../api/paginations";
 import BodyPage from "./BodyPage";
 import BookMarked from "./BookMarked/BookMarked";
@@ -5,15 +6,22 @@ import Films from "./Films/Films";
 import HeaderTestimonial from "./testimonial/HeaderTestimonial";
 
 const MainPage = () => {
-    return (
-       <div className="bg-gray-900  overflow-hidden max-w-full w-full px-6">
-            <HeaderTestimonial />
-            <BodyPage />
-            {/* <Films/> */}
-            <BookMarked/>
-            <button className="bg-cyan-400" onClick={() => paginations()}>PAGINATION</button>
-       </div>
-    );
+  const [openFilms, setOpenFilms] = useState(true);
+
+  const openItem = () => {
+    setOpenFilms((films) => !films);
+  };
+
+  return (
+    <div className="bg-gray-900 overflow-hidden max-w-full w-full px-6">
+      <HeaderTestimonial />
+      <BodyPage openClickedFilms={openItem} openFilms={openFilms} />
+      {openFilms ? <Films /> : <BookMarked />}
+      <button className="bg-cyan-400" onClick={() => paginations()}>
+        PAGINATION
+      </button>
+    </div>
+  );
 };
 
 export default MainPage;
