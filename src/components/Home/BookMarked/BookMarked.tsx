e55bee7740a8cbab39e68bccac76da25dfb95f6e
Sasa Nedic <svetasmak0@gmail.com>
2024-10-01 19:53:52 +0200

    Create some changes in style

--- src/components/Home/BookMarked/BookMarked.tsx
+++ src/components/Home/BookMarked/BookMarked.tsx
@@ -7,13 +7,13 @@ const BookMarked = () => {
 
   return (
     <div className="max-w-[71rem] mx-auto mt-14">
-      <div className="grid grid-cols-4 gap-6">
+      <div className="grid grid-cols-4 gap-6 ">
         {bookedFilm.length > 0 ? (
           bookedFilm.map((film) => (
             <CardTestimonial key={film.id2} testimonialFilms={film} />
           ))
         ) : (
-          <p className="bg-slate-100">No marked films.</p>
+          <p className="text-center pl-12 text-xl text-gray-400">No marked films.</p>
         )}
       </div>
     </div>
