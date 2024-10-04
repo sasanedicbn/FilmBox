79518bb0e4f5510de9de66097f05a6e483c3c2e7
Sasa Nedic <svetasmak0@gmail.com>
2024-10-04 21:50:08 +0200

    Create changes in LoginForm component

--- src/features/auth/Login/LoginForm.tsx
+++ src/features/auth/Login/LoginForm.tsx
@@ -36,9 +36,6 @@ const LoginForm = () => {
         <Label text="Email" />
         <Input
           type="email"
-          {...register('email')}
-          styleType="login"
-        />
         {errors.email && (
           <p className="text-red-500 text-sm mt-1">{errors.email.message}</p>
         )}
