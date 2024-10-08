078d9ca491779d8d5041b6d85b857c1611ede3f5
Sasa Nedic <svetasmak0@gmail.com>
2024-10-08 16:19:02 +0200

    Create changes in Login component

--- src/features/Login.tsx
+++ src/features/Login.tsx
@@ -1,16 +1,33 @@
+import { useState } from "react";
 import LoginHeader from "./LoginHeader";
 import Signup from "./Signup";
+import LoginForm from "./LoginForm";
 
 const Login = () => {
+  const [openSignUp, setOpenSignUp] = useState(false);
+
+  const openSignUpHandler = () => {
+    setOpenSignUp((state) => !state);
+  };
+
   return (
     <div className="h-screen flex justify-center items-center w-full bg-gray-800">
       <div className="bg-gray-300 w-[425px] h-[95%] flex flex-col justify-center p-10 rounded-lg shadow-lg">
         <LoginHeader />
-        {/* <LoginForm /> */}
-        <Signup/>
+        {openSignUp ? <LoginForm /> : <Signup />}
         <div className="mt-4 text-center text-gray-800">
-          <p>Don't have an account? <a href="#" className="text-blue-700">Sign Up</a></p>
-          <p className="mt-2">Or, <a href="#" className="text-blue-700">Log in as guest</a></p>
+          <p>
+            Don't have an account?{" "}
+            <a href="#" className="text-blue-700" onClick={openSignUpHandler}>
+              Sign Up
+            </a>
+          </p>
+          <p className="mt-2">
+            Or,{" "}
+            <a href="#" className="text-blue-700">
+              Log in as guest
+            </a>
+          </p>
         </div>
       </div>
     </div>
