7149ef2b1ab554991c3395bfaf3562942c14bdd2
Sasa Nedic <svetasmak0@gmail.com>
2024-10-13 23:04:07 +0200

    Fix some ux design

--- src/components/Home/testimonial/CardTestimonial.tsx
+++ src/components/Home/testimonial/CardTestimonial.tsx
@@ -5,7 +5,7 @@ const CardTestimonial = ({ testimonialFilms }: { testimonialFilms: Film }) => {
   return (
     <div className="w-full h-80 bg-gray-900 flex justify-center items-center overflow-hidden relative rounded-lg">
       <img
-        className="h-80 w-full object-cover"
+        className="h-80 w-50 object-cover"
         src={testimonialFilms.image}
         alt="Testimonial"
       />
