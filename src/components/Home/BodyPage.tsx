df27eb23da5764a0ffabfa0fbe540ec0b2f56845
Sasa Nedic <svetasmak0@gmail.com>
2024-10-13 12:57:51 +0200

    Clean code in BodyPage component

--- src/components/Home/BodyPage.tsx
+++ src/components/Home/BodyPage.tsx
@@ -6,56 +6,44 @@ import { BodyPageProps } from "../../types/types";
 import { collection, getDocs, query, where, orderBy } from "firebase/firestore";
 import { db } from "../../config/firebase";
 import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { setFilms } from "../../store/slices/filmsSlice";
 
 const BodyPage = ({ openClickedFilms, openFilms }: BodyPageProps) => {
   const [genre, setGenre] = useState<string>("all");
   const dispatch = useDispatch();
-  const films = useSelector((state) => state.films.films);
-  // console.log("films", films);
-
-
 
   const fetchSortedFilms = async () => {
     try {
       const filmsRef = collection(db, "films");
 
       let q;
-     console.log('q', q)
+
       if (genre === "all") {
-        // Ako je žanr "all", prikazujemo sve filmove
         q = query(filmsRef, orderBy("title", "asc"));
       } else {
-        // Ako je izabran specifičan žanr, filtriramo prema njemu
         q = query(
           filmsRef,
-          where("genre", "array-contains", genre), // Pretpostavimo da polje 'genre' sadrži niz žanrova
+          where("genre", "array-contains", genre), 
           orderBy("genre")
         );
       }
 
       const querySnapshot = await getDocs(q);
-      console.log('querySnapshot', querySnapshot)
-      // ovdje mi vraca svih 100 filmova znaci gore se ono ne uradi kako treba
       const filteredFilms = querySnapshot.docs.map((doc) => {
         const filmData = doc.data();
-        // console.log("Film data:", filmData);
         return {
           id: doc.id,
           ...filmData,
         };
       });
 
-      // console.log("sortirano kao", genre);
-      console.log(filteredFilms)
       dispatch(setFilms(filteredFilms));
     } catch (error) {
       console.error("Error fetching films:", error);
     }
   };
 
-  // Funkcija koja se poziva kada se promeni izabrani žanr
   const handleGenreChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedGenre = e.target.value;
     setGenre(selectedGenre);
@@ -81,7 +69,7 @@ const BodyPage = ({ openClickedFilms, openFilms }: BodyPageProps) => {
       <div className="flex items-center gap-4">
         <Select
           name="genreSelect"
-          onChange={handleGenreChange} // Poziva handleGenreChange funkciju prilikom promene
+          onChange={handleGenreChange} 
         >
           <Option value="all">All</Option>
           <Option value="Action">Action</Option>
