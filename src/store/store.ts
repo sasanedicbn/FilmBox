71836bd76a0aa0817e3ab103abdcbe95e0afebf0
Sasa Nedic <svetasmak0@gmail.com>
2024-10-14 21:23:58 +0200

    Add in store loading

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import filmsSlice from './slices/filmsSlice';
+import loadingSlice from './slices/loadingSlice';
 
 export const store = configureStore({
   reducer: {
     films: filmsSlice, 
+    loading: loadingSlice,
   },
 });
 
