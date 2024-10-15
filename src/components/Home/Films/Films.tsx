299e3d1d036108fe448c8b1c8f2e416edf9d85b9
Sasa Nedic <svetasmak0@gmail.com>
2024-10-15 18:10:40 +0200

    Problem with pagination

--- src/components/Home/Films/Films.tsx
+++ src/components/Home/Films/Films.tsx
@@ -1,77 +1,98 @@
 import { useEffect, useState } from "react";
 import CardTestimonial from "../testimonial/CardTestimonial";
 import FilmsDetails from "./FilmsDetails";
-import { collection, getDocs, limit, orderBy, query, startAfter, startAt } from "firebase/firestore";
+import { collection, endBefore, getDocs, limit, query, startAfter } from "firebase/firestore";
 import { db } from "../../../config/firebase";
 import { useDispatch, useSelector } from "react-redux";
 import { setFilms } from "../../../store/slices/filmsSlice";
 
 const Films = () => {
-  const [pageHistory, setPageHistory] = useState([]); // Istorija paginacije
-  const [currentPageIndex, setCurrentPageIndex] = useState(0); // Trenutni indeks stranice
+  const [currentPage, setCurrentPage] = useState(0);
+  const [lastVisible, setLastVisible] = useState(null);
+  const [firstVisible, setFirstVisible] = useState(null);
   const films = useSelector(state => state.films.films);
   const dispatch = useDispatch();
 
-  const fetchMovies = async (startAfterDoc = null) => {
+  const fetchNextPage = async () => {
     const coll = collection(db, "films");
-    let moviesQuery;
-
-    if (startAfterDoc) {
-      moviesQuery = query(coll, orderBy("rating", "desc"), startAfter(startAfterDoc), limit(12));
-    } else {
-      moviesQuery = query(coll, orderBy("rating", "desc"), limit(12));
+  
+    const moviesQuery = lastVisible 
+      ? query(coll, startAfter(lastVisible), limit(12))
+      : query(coll, limit(12)); 
+  
+    const data = await getDocs(moviesQuery);
+  
+    if (data.empty) {
+      console.log('Nema više podataka');
+      return;
     }
+  
+    const movies = data.docs.map((doc) => ({ id2: doc.id, ...doc.data() }));
+    // console.log('prvi okret ->>>>', movies);
+  
+    dispatch(setFilms(movies)); 
+  
+    const lastVisibleRef = data.docs[data.docs.length - 1]; 
+    // console.log('lastVisibleRef', lastVisibleRef);
+    setLastVisible(lastVisibleRef);
+  
+    const firstVisibleRef = data.docs[0]; 
+
+    // console.log('firstVisibleRef OOOOVAJ', firstVisibleRef);
+    setFirstVisible(firstVisibleRef);
+  
+    setCurrentPage((currentPage) => currentPage + 1);
+  };
+
+  console.log('firstvisible', firstVisible?.id)
+  const fetchPreviousPage = async () => {
+    if (!firstVisible) return; 
+
+    const coll = collection(db, "films");
+    const moviesQuery = query(coll, endBefore(firstVisible), limit(12)); 
 
     const data = await getDocs(moviesQuery);
+
+    //  console.log('DATA', data)
     if (data.empty) {
-      console.log("Nema više podataka");
+      console.log('Nema više podataka');
       return;
     }
 
-    const movies = data.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+    const movies = data.docs.map((doc) => ({ id2: doc.id, ...doc.data() }));
+    console.log('prethodni okret ->>>>', movies);
+
     dispatch(setFilms(movies));
 
-    // Ažuriraj istoriju stranica
-    const lastDoc = data.docs[data.docs.length - 1];
-    if (startAfterDoc) {
-      setPageHistory((prevHistory) => [...prevHistory, lastDoc]);
-    } else {
-      setPageHistory([lastDoc]); // Resetuj istoriju prilikom prvog učitavanja
-    }
-  };
+    const lastVisibleRef = data.docs[data.docs.length - 1]; 
+    // console.log('lastVisibleRef', lastVisibleRef);
+    setLastVisible(lastVisibleRef);
 
-  const fetchNextPage = async () => {
-    if (pageHistory.length === 0 || currentPageIndex >= pageHistory.length - 1) return;
-    const nextDoc = pageHistory[currentPageIndex];
-    await fetchMovies(nextDoc);
-    setCurrentPageIndex((prevIndex) => prevIndex + 1);
-  };
+    const firstVisibleRef = data.docs[0]; 
+    // console.log('firstVisibleRef', firstVisibleRef);
+    setFirstVisible(firstVisibleRef);
 
-  const fetchPreviousPage = async () => {
-    if (currentPageIndex <= 0) return;
-    const prevDoc = pageHistory[currentPageIndex - 1];
-    await fetchMovies(prevDoc);
-    setCurrentPageIndex((prevIndex) => prevIndex - 1);
+    setCurrentPage((currentPage) => currentPage - 1); 
   };
 
   useEffect(() => {
-    fetchMovies(); // Učitaj prvu stranicu pri montiranju
+    fetchNextPage(); 
   }, []);
 
   return (
     <div className="max-w-[71rem] mx-auto mt-14">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2  md:grid-cols-3 lg:grid-cols-4 gap-6">
         {films.map((film) => (
-          <div key={film.id} className="flex flex-col">
+          <div key={film.id2} className="flex flex-col">
             <CardTestimonial testimonialFilms={film} />
             <FilmsDetails films={film} />
           </div>
         ))}
       </div>
-      <button className="bg-slate-50 mr-4" onClick={fetchPreviousPage} disabled={currentPageIndex === 0}>
+      <button className="bg-slate-50 mr-4" onClick={fetchPreviousPage}>
         Previous
       </button>
-      <button className="bg-slate-50 mr-4" onClick={fetchNextPage} disabled={currentPageIndex >= pageHistory.length - 1}>
+      <button className="bg-slate-50 mr-4" onClick={fetchNextPage}>
         Next
       </button>
     </div>
