9e358b95376b4e35a95ab35689148d646af0ade3
Sasa Nedic <svetasmak0@gmail.com>
2024-10-16 22:48:12 +0200

    Clean code in Pagitnation component

--- src/components/Home/Films/Pagination.tsx
+++ src/components/Home/Films/Pagination.tsx
@@ -1,21 +1,18 @@
 import { useState } from "react";
-import { useSelector } from "react-redux";
 import { fetchTotalFilmsCount } from "../../../api/fetchTotalFIlmsCount";
 
 const Pagination = ({fetchNextPage, fetchPreviousPage, fetchPage}) => {
     const [numPagination, setNumPagination] = useState(0)
+    const [activePage, setActivePage] = useState(0); 
+    let lengthPagination = Math.ceil(numPagination / 12);
+    
     const totalFilms = async () => {
         const filmsCount = await fetchTotalFilmsCount()
         console.log('filmsCount',filmsCount)
         setNumPagination(filmsCount)
     }
     totalFilms()
-    const [activePage, setActivePage] = useState(0); 
-    const films = useSelector((film) => film.films.films)
-    // console.log('iz paginacije',films.len)
-    let lengthPagination = Math.ceil(numPagination / 12);
-    console.log('numPagination', numPagination)
-    console.log('lengthPagination', lengthPagination)
+
 
     const handleNext = () => {
         if (activePage < lengthPagination - 1) {
