c97f729ddc21fae8d370ec48de0f035f5018e88b
Sasa Nedic <svetasmak0@gmail.com>
2024-10-16 15:51:39 +0200

    Fix some error

--- src/components/Home/Films/Films.tsx
+++ src/components/Home/Films/Films.tsx
@@ -80,7 +80,7 @@ const Films = () => {
     <div className="max-w-[71rem] mx-auto mt-14">
       <div className="grid grid-cols-1 sm:grid-cols-2  md:grid-cols-3 lg:grid-cols-4 gap-6">
         {films.map((film) => (
-          <div key={film.id2} className="flex flex-col">
+          <div key={film.id} className="flex flex-col">
             <CardTestimonial testimonialFilms={film} />
             <FilmsDetails films={film} />
           </div>
