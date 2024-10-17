682847a2389c175cd51046d3962b1ce16e659a1d
Sasa Nedic <svetasmak0@gmail.com>
2024-10-17 14:19:47 +0200

    Add FilmsDetails in BookMarked component

--- src/components/Home/BookMarked/BookMarked.tsx
+++ src/components/Home/BookMarked/BookMarked.tsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
 import CardTestimonial from "../testimonial/CardTestimonial";
+import FilmsDetails from "../Films/FilmsDetails";
 
 const BookMarked = () => {
   const bookedFilm = useSelector((state) => state.films.markedFilms);
@@ -10,7 +11,10 @@ const BookMarked = () => {
       <div className="grid grid-cols-4 gap-6 ">
         {bookedFilm.length > 0 ? (
           bookedFilm.map((film) => (
-            <CardTestimonial key={film.id2} testimonialFilms={film} />
+            <div key={film.id} className="flex flex-col">
+             <CardTestimonial key={film.id2} testimonialFilms={film} />
+             <FilmsDetails films={film} />
+            </div>
           ))
         ) : (
           <p className="text-center pl-12 text-xl text-gray-400">No marked films.</p>
