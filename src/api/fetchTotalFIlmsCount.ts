66725811df564d9025c57c97738d43955c712c73
Sasa Nedic <svetasmak0@gmail.com>
2024-10-18 23:02:48 +0200

    Change code

--- src/api/fetchTotalFIlmsCount.ts
+++ src/api/fetchTotalFIlmsCount.ts
@@ -4,5 +4,4 @@ import { db } from "../config/firebase";
 export const fetchTotalFilmsCount = async () => {
     const coll = collection(db, "films");
     const snapshot = await getCountFromServer(coll);
-    return snapshot.data().count;
-};
+ 
