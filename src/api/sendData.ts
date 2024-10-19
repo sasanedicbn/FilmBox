07b73eff27438ad7b6747ee24402f075fcb817ad
Sasa Nedic <svetasmak0@gmail.com>
2024-10-19 13:31:29 +0200

    Create changes in sendData function

--- src/api/sendData.ts
+++ src/api/sendData.ts
@@ -1,8 +1,9 @@
 import { collection, addDoc } from 'firebase/firestore';
 import { db } from '../config/firebase';
 import { toast } from 'react-toastify';
+import { Film } from '../types/types';
 
-export const sendData = async (films) => {
+export const sendData = async (films:Film[]) => {
   try {
     if(films){
         for(const film of films){
