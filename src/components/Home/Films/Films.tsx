8fd361882d08feec374a2e1d89694626e4b67e1e
Sasa Nedic <svetasmak0@gmail.com>
2024-10-19 13:59:41 +0200

    Create changes in Films component

--- src/components/Home/Films/Films.tsx
+++ src/components/Home/Films/Films.tsx
@@ -3,16 +3,18 @@ import FilmsDetails from "./FilmsDetails";
 import Pagination from "./Pagination";
 import useFilmsPagination from "../../custom-hook/useFilmsPagination";
 import { useSelector } from "react-redux";
+import { RootState } from "../../../store/slices/filmsSlice";
+import { Film } from "../../../types/types";
 
 const Films = () => {
-  const films = useSelector((state) => state.films.films);
+  const films = useSelector((state:RootState) => state.films.films);
 
   const { fetchNextPage, fetchPreviousPage, fetchPage } = useFilmsPagination();
 
   return (
     <div className="max-w-[71rem] mx-auto mt-14">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {films.map((film) => (
+        {films.map((film:Film) => (
           <div key={film.id} className="flex flex-col">
             <CardTestimonial testimonialFilms={film} />
             <FilmsDetails films={film} />
