83c9e0841313416125a560309b6a7785bd210c82
Sasa Nedic <svetasmak0@gmail.com>
2024-10-26 21:17:20 +0200

    Create changes

--- src/components/UI/HoverComponent.tsx
+++ src/components/UI/HoverComponent.tsx
@@ -35,12 +35,7 @@ const HoverComponent = ({ films }: { films: Film }) => {
   return (
     <div className="w-[220px] mx-auto absolute inset-0 bg-slate-400 pointer bg-opacity-60 flex flex-col justify-center items-center opacity-0 hover:opacity-100 transition-opacity duration-300 p-2">
       <div className="text-center">
-        <span className="absolute top-2 right-2">
-          <Icon name="marked" size={30} color="white" onClick={handleMarkFilm} type="pointer" />
-        </span>
-        <button>See more</button> 
-      </div>
-    </div>
+        <span className="absolute top-2 
   );
 };
 
