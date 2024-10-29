a58ad93a17f40e97e777a9f070991ff5feeb5137
Sasa Nedic <svetasmak0@gmail.com>
2024-10-29 22:41:20 +0100

    Create changes in HeaderSingleFilm

--- src/components/SingleFilm/HeaderSingleFilm.tsx
+++ src/components/SingleFilm/HeaderSingleFilm.tsx
@@ -1,8 +1,13 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../../store/slices/filmsSlice";
+import { Film } from "../../types/types";
 
 const HeaderSingleFilm = () => {
-  const film = useSelector((state:RootState) => state.films?.currentFilm); 
+  const film = useSelector((state: RootState) => state.films?.currentFilm) as Film | null;
+
+  if (!film) {
+    return <p>Film details not available.</p>;
+  }
 
   return (
     <div className="flex justify-between mt-4">
@@ -10,9 +15,13 @@ const HeaderSingleFilm = () => {
         <h3 className="text-1xl font-bold uppercase">{film.title}</h3>
         <p className="text-md text-gray-400">({film.year})</p>
       </div>
-       <div className="text-right ">
-        <p className="text-gray-300 ">IMDb Rating: <span className="font-semibold">{film.rating}/10</span></p>
-        <p className="text-gray-300 ">Rank: <span className="font-semibold w-[100%]">{film.rank}/100</span></p>
+      <div className="text-right">
+        <p className="text-gray-300">
+          IMDb Rating: <span className="font-semibold">{film.rating}/10</span>
+        </p>
+        <p className="text-gray-300">
+          Rank: <span className="font-semibold w-[100%]">{film.rank}/100</span>
+        </p>
       </div>
     </div>
   );
