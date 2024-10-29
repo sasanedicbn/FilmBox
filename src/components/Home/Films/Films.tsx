cf561cc88b9237a1e08b11c11123eca90a78a615
Sasa Nedic <svetasmak0@gmail.com>
2024-10-29 16:47:22 +0100

    Add Spinner in Films component

--- src/components/Home/Films/Films.tsx
+++ src/components/Home/Films/Films.tsx
@@ -7,6 +7,7 @@ import { RootState } from "../../../store/slices/filmsSlice";
 import { Film } from "../../../types/types";
 import Button from "../../UI/Button";
 import useFilmsGenre from "../../custom-hook/useFilmsGenre";
+import Spinner from "../../UI/Spinner";
 
 const Films = () => {
   const films = useSelector((state: RootState) => state.films.films);
@@ -15,34 +16,45 @@ const Films = () => {
   const { fetchNextPage, fetchPreviousPage, fetchPage } = useFilmsPagination();
   const { fetchSortedFilms } = useFilmsGenre();
 
-  console.log(films, "availableFilms");
+  const loading = films.length === 0;
 
   return (
     <div className="max-w-[71rem] mx-auto mt-14">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {films.map((film: Film) => (
-          <div key={film.id} className="flex flex-col">
-            <CardTestimonial testimonialFilms={film} />
-            <FilmsDetails films={film} />
-          </div>
-        ))}
-      </div>
-      {genre !== null ? (
-        <div className="flex justify-center my-4">
-          <Button
-            type="upload"
-            disabled={films.length >= totalCount}
-            onClick={() => fetchSortedFilms(true)}
-          >
-            {`Upload more ${genre ? genre.toLowerCase() : ""} movies`}
-          </Button>
+      {loading ? (
+        <div className="flex justify-center my-8">
+          <Spinner />
         </div>
       ) : (
-        <Pagination
-          fetchNextPage={fetchNextPage}
-          fetchPreviousPage={fetchPreviousPage}
-          fetchPage={fetchPage}
-        />
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {films.map((film: Film) => (
+            <div key={film.id} className="flex flex-col">
+              <CardTestimonial testimonialFilms={film} />
+              <FilmsDetails films={film} />
+            </div>
+          ))}
+        </div>
+      )}
+
+      {!loading && (
+        <>
+          {genre !== null ? (
+            <div className="flex justify-center my-4">
+              <Button
+                type="upload"
+                disabled={films.length >= totalCount}
+                onClick={() => fetchSortedFilms(true)}
+              >
+                {`Upload more ${genre ? genre.toLowerCase() : ""} movies`}
+              </Button>
+            </div>
+          ) : (
+            <Pagination
+              fetchNextPage={fetchNextPage}
+              fetchPreviousPage={fetchPreviousPage}
+              fetchPage={fetchPage}
+            />
+          )}
+        </>
       )}
     </div>
   );
