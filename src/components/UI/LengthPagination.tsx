952e78806c62ead9c6a1221a3a1844b18da2ebfb
Sasa Nedic <svetasmak0@gmail.com>
2024-10-31 16:39:57 +0100

    Create mini responsive in paginationwrapper

--- src/components/UI/LengthPagination.tsx
+++ src/components/UI/LengthPagination.tsx
@@ -6,7 +6,7 @@ const LengthPagination = ({ lengthPagination, activePage, handlePageChange }:Len
             {Array.from({ length: lengthPagination }).map((_, index) => (
                 <div
                     key={index}
-                    className={`w-8 h-8 flex justify-center items-center rounded-full cursor-pointer font-bold transition-all duration-300 
+                    className={`w-8 h-8  flex justify-center items-center rounded-full cursor-pointer font-bold transition-all duration-300 lg:w-4 h-8
                     ${activePage === index + 1 ? 'bg-cyan-600 text-white' : 'bg-cyan-200 text-black hover:bg-cyan-400'}`}
                     onClick={() => handlePageChange(index + 1)} 
                 >
--- src/components/UI/PaginationWrapper.tsx
+++ src/components/UI/PaginationWrapper.tsx
@@ -1,17 +1,16 @@
 import { PaginationWrapperProps } from "../../types/types";
 
-
-const PaginationWrapper = ({children, type}:PaginationWrapperProps) => {
-    const base = 'flex justify-between my-8 mx-auto'
+const PaginationWrapper = ({ children, type }: PaginationWrapperProps) => {
+    const base = 'flex justify-between my-8 mx-auto';
     const styles = {
-        smallNumberPagination: `${base} w-1/4`,
-        bigNumberPagination: `${base} w-1/2`,
-      };
-    return(
+        smallNumberPagination: `${base} w-full sm:w-1/2 md:w-1/3 lg:w-1/4`, 
+        bigNumberPagination: `${base} w-full sm:w-3/4 md:w-2/3 lg:w-1/2`,
+    };
+    return (
       <div className={styles[type]}>
         {children}
       </div>
-    )
-}
+    );
+};
 
 export default PaginationWrapper;
